40a5dd094983ec6d255bd0d9470f7c4cb7f3bb46
Bernd Edlinger <bernd.edlinger@hotmail.de>
2017-09-21 16:14:18 +0200

    Add support for multiple SD cards on one host interface.
    Needs external logic to multiplex the clock signal,
    which is controlled by a GPIO output pin.
    
    Only the selected SD card receives the clock,
    while the other SD card's clock is forced to LOW.
    DATA and CMD lines can be connected to both SD cards.
    
    On warm-start UBOOT should use a sequence of
    CMD12 and CMD7 with RCA=0 to force any active SD card
    to stop transmission and release the DATA bus.
    
    Instantiate this in the device tree as:
    
     &mmc {
      select-gpio = <&portd 2 0>;
      cd-gpios = <&portb 28 1>,
                 <&porta 18 1>;
     };
    
    Signed-off-by: Bernd Edlinger <bernd.edlinger@hotmail.de>

--- drivers/mmc/host/dw_mmc.h
+++ drivers/mmc/host/dw_mmc.h
@@ -20,6 +20,8 @@
 #include <linux/reset.h>
 #include <linux/interrupt.h>
 
+#define MAX_MCI_SLOTS	2
+
 enum dw_mci_state {
 	STATE_IDLE = 0,
 	STATE_SENDING_CMD,
@@ -109,7 +111,8 @@ struct dw_mci_dma_slave {
  * @priv: Implementation defined private data.
  * @biu_clk: Pointer to bus interface unit clock instance.
  * @ciu_clk: Pointer to card interface unit clock instance.
- * @slot: Slots sharing this MMC controller.
+ * @slot: The slot which is currently using the controller.
+ * @slots: Slots sharing this MMC controller.
  * @fifo_depth: depth of FIFO.
  * @data_addr_override: override fifo reg offset with this value.
  * @wm_aligned: force fifo watermark equal with data length in PIO mode.
@@ -127,6 +130,7 @@ struct dw_mci_dma_slave {
  * @cmd11_timer: Timer for SD3.0 voltage switch over scheme.
  * @cto_timer: Timer for broken command transfer over scheme.
  * @dto_timer: Timer for broken data transfer over scheme.
+ * @card_select: The optional external card select output.
  *
  * Locking
  * =======
@@ -202,6 +206,7 @@ struct dw_mci {
 
 	u32			bus_hz;
 	u32			current_speed;
+	u32			num_slots;
 	u32			fifoth_val;
 	u16			verid;
 	struct device		*dev;
@@ -211,6 +216,7 @@ struct dw_mci {
 	struct clk		*biu_clk;
 	struct clk		*ciu_clk;
 	struct dw_mci_slot	*slot;
+	struct dw_mci_slot	*slots[MAX_MCI_SLOTS];
 
 	/* FIFO push and pull */
 	int			fifo_depth;
@@ -234,6 +240,8 @@ struct dw_mci {
 	struct timer_list       cmd11_timer;
 	struct timer_list       cto_timer;
 	struct timer_list       dto_timer;
+
+	struct gpio_desc	*card_select;
 };
 
 /* DMA ops for Internal/External DMAC interface */
